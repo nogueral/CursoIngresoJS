e5f7014b50ab09511b49d5dd0e496b0a292d8319
Leandro Noguera <nogueraleandro@hotmail.com>
2020-02-05 16:57:59 -0300

    Resolucion ejerc jsEntradaSalida 3 al 10

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,22 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+        var numeroUno;
+        var numeroDos;
+        var resultado;
 
+        numeroUno = document.getElementById("numeroUno").value; 
+        numeroDos = document.getElementById("numeroDos").value; 
+
+        numeroUno = parseInt(numeroUno);
+        numeroDos = parseInt(numeroDos);
+
+        resultado = numeroUno + numeroDos;
+
+        alert(resultado);
+
+        console.log(numeroUno);
+        console.log(numeroDos);
+        console.log(resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo
+    var resultado
 
+    sueldo = document.getElementById("sueldo").value;
+    
+    sueldo = parseInt(sueldo);
+
+    resultado = sueldo * 1.1;
+
+    document.getElementById("resultado").value = resultado;
 	
 }
