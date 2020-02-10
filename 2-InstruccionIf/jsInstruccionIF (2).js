a8a2adb9552257476788a26afbb20b560a198f2d
alumno <alumno@utnfra.com.ar>
2020-02-10 20:30:51 -0300

    Ej Instruccion IF 1, 2, 3 y 4

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,12 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad = document.getElementById("edad").value;
 
+if(edad >= "18") {
+
+    alert ("La persona es mayor de edad")
+
+    }
 
 }//FIN DE LA FUNCIÓN
