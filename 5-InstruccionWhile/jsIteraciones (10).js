934af0a0310e2312441068a640f78c37c3110433
LeandroNoguera <nogueraleandro@hotmail.com>
2020-02-24 14:45:03 -0300

    Ej 10 Instruccion While

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,15 +1,57 @@
+/*Al presionar el botón pedir números hasta que el usuario quiera, mostrar:
+1-Suma de los negativos. 2-Suma de los positivos. 3-Cantidad de positivos. 4-Cantidad de negativos. 
+5-Cantidad de ceros. 6-Cantidad de números pares. 7-Promedio de positivos. 8-Promedios de negativos. 
+9-Diferencia entre positivos y negativos, (positvos-negativos).*/
+
+
 function mostrar()
 {
 
 	var contador=0;
+	var contadorPositivos=0;
+	var contadorNegativos=0;
+	var contadorCeros=0;
 	//declarar contadores y variables 
-	
-	var respuesta="si";
+	var numeroIngresado;
+	var positivos=0;
+	var negativos=0;
+	var respuesta=true;
+	var contadorPares=0;
+	var resto;
+	var promedioPositivos=0;
+	var promedioNegativos=0;
 
-	while(respuesta!="no")
+
+	while(respuesta == true)
 	{
+		numeroIngresado = prompt("Ingrese un numero: ");
+		numeroIngresado = parseInt(numeroIngresado);
+
+		if (numeroIngresado < 0) {
+			negativos = negativos + numeroIngresado;
+			contadorNegativos++;
+			promedioNegativos = negativos / contadorNegativos;
+
+		} else if (numeroIngresado > 0) {
+			positivos = positivos + numeroIngresado;
+			contadorPositivos++;
+			resto = numeroIngresado % 2;
+			promedioPositivos = positivos / contadorPositivos;
+
+			if (resto == 0) {
+				contadorPares++;
+			}
+
+		} else if (numeroIngresado == 0) {
+			contadorCeros = contadorCeros + 1;
+		}
+
 		
-	
+		
+
+		console.log(numeroIngresado, contadorPositivos, contadorNegativos, promedioPositivos, promedioNegativos);
+
+		respuesta = confirm("Desea ingresar otro numero?: ");
 	}
 
 
