f04bdf6ffb3e93716ec67bb8f131eb2923f2ee1a
LeandroNoguera <nogueraleandro@hotmail.com>
2020-02-27 12:24:04 -0300

    Parciales ej 8

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -15,13 +15,26 @@ function mostrar()
     var numero;
     var letra;
     var numerosPares;
-    var contadorPares;
+    var contadorPares=0;
+    var numerosImpares;
+    var contadorImpares=0;
+    var contadorCeros=0;
+    var numerosPositivos=0;
+    var contadorPositivos=0;
+    var promedioPositivos=0;
+    var numerosNegativos=0;
+    var flag=true;
+    var minimo;
+    var maximo;
+    var letraMinimo;
+    var letraMaximo;
 
     while(respuesta==true) {
 
         numero = prompt("Ingrese un numero entre -100 y 100: ");
         numero = parseInt(numero);
-            while(numero < -100 || numero > 100) {
+
+            while(numero < -100 || numero > 100 || isNaN(numero)) {
                  numero = prompt("Error! Ingrese un numero entre -100 y 100: ");
                  numero = parseInt(numero);
         }
@@ -29,13 +42,55 @@ function mostrar()
         letra = letra.toLowerCase(letra);
 
         numerosPares = numero % 2;
-        if (numerosPares==0) {
+        numerosImpares = numero % 2;
+
+        if (numerosPares==0 && numero!=0) {
             contadorPares++;
-            console.log(contadorPares);
+        
+        }   else if (numerosImpares==1) {
+            contadorImpares++;
+            
+        }   else if (numero==0) {
+            contadorCeros++;
+        }   
+        
+        if (numero > 0) {
+        contadorPositivos++;
+        numerosPositivos = numerosPositivos + numero;
+            
+        }   else if (numero < 0) {
+            numerosNegativos = numerosNegativos + numero;
+
         }
 
+        if (flag==true) {
+            minimo = numero;
+            letraMinimo = letra;
+            maximo = numero;
+            letraMaximo = letra;
+            flag = false;
+       
+        }   else if (numero < minimo) {
+            minimo = numero;
+            letraMinimo = letra;
+        
+        }   else if (numero > maximo) {
+            maximo = numero;
+            letraMaximo = letra;
+        }
 
+        
         respuesta = confirm("Desea ingresar otro dato?")
     }
+
+        promedioPositivos = numerosPositivos / contadorPositivos;
+        console.log(maximo, letraMaximo, minimo, letraMinimo);
+
+        document.write("a- Cantidad numeros pares: "+ contadorPares + "<br>", "b- Cantidad numeros impares: "+
+        contadorImpares + "<br>", "c- Cantidad ceros: "+ contadorCeros + "<br>", 
+        "d- Promedio positivos: "+ promedioPositivos + "<br>", "e- Suma de negativos: "+
+        numerosNegativos + "<br>", "f- Numero y letra maximo: "+ maximo + letraMaximo + "<br>",
+        "g- Numero y letra minimo: " + minimo + letraMinimo + "<br>");
+
 
 }
